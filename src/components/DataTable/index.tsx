f6e898d23103d510b9cb0bba19471b32cf2d4a90
Luan Oliveira <luanoliveira@Luans-MacBook-Pro.local>
2024-08-05 19:01:17 -0300

    irst commit

---
+++ src/components/DataTable/index.tsx
@@ -0,0 +1,26 @@
+interface DataProps {
+  createdDT: string;
+  modifiedDT: string;
+  entity: string;
+  operatingStatus: string;
+  legalName: string;
+  dbaName: string;
+  physicalAddress: string;
+  phone: string;
+  dot: string;
+  mcMxFf: string;
+  powerUnits: string;
+  outOfServiceDate: string;
+}
+
+// const columns: Column<DataProps>[] = [
+//     {}
+// ]
+
+export default function DataTable() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+}
